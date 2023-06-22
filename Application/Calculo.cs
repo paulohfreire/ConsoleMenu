eb5e6643adf2c89a5fabf5d93ce8f98cfb6f8b63
paulohfreire <paulo.freire53@gmail.com>
2023-06-22 20:28:29 -0300

    Nessa atividade foram feitas alterações no program.cs para receber o objeto Cliente que herda do objeto Pessoa  as características de nome e implementa saldo que é a propriedade a qual iremos trabalhar para aplicar os cálculos de depósito e saque da conta que foram implementados no arquivo Calculo.cs. Nessa implementação, ao entrar no sistema o usuário insere seu nome e informa o valor  do seu saldo. Em seguida escolhe a operação de depositar ou sacar e o programa efetua as operações.

---
+++ Application/Calculo.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleMenu.Model.Domain;
+
+namespace ConsoleMenu.Application
+{
+    public class Calculo
+    {
+        public void Deposito(Cliente cliente, float valor)
+        {
+            cliente.Saldo += valor;
+        }
+
+        public void Saque(Cliente cliente, float valor)
+        {
+            if (valor <= cliente.Saldo)
+            {
+                cliente.Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Você não possui saldo suficiente.");
+            }
+
+        }
+    }
+}
---
+++ Model/Domain/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenu.Model.Domain
+{
+    public class Cliente : Pessoa
+    {
+        public int Conta { get; set; }
+        public float Saldo { get; set; }
+    }
+}
