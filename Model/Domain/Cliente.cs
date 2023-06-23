80567c99ac37a41756943a36a3227746a1206c24
paulohfreire <paulo.freire53@gmail.com>
2023-06-23 15:19:00 -0300

    Nessa etapa foi adicionado o pacote de validação de CPF do nuGet CPF.CNPJ, e foram feitas as validações dos campos de cpf, id e saldo. Além disso foi criada uma lista para armazenar as mensagens de erro, quando ocorrerem.

--- Model/Domain/Cliente.cs
+++ Model/Domain/Cliente.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleMenu.Model.Domain
+﻿namespace ConsoleMenu.Model.Domain
 {
     public class Cliente : Pessoa
     {
--- Model/Domain/Pessoa.cs
+++ Model/Domain/Pessoa.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleMenu.Model.Domain
+﻿namespace ConsoleMenu.Model.Domain
 {
     public class Pessoa
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public long Cpf { get; set;}
     }
 }
